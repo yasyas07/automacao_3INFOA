049fb43da652cfd227590291f2f9ca032db2c2bd
Ivan Pereira <softip@gmail.com>
2024-02-19 10:13:37 -0300

    Aula da semana 1

---
+++ semana1/exemplo2_entrada.py
@@ -0,0 +1,5 @@
+#Entrada
+n1 = input("Digite o número 1: ")
+n2 = input("Digite o número 2: ")
+#Saída
+print("Você digitou", n1, n2)
---
+++ semana1/exemplo3_processamento.py
@@ -0,0 +1,13 @@
+#Entrada
+n1 = input("Digite o número 1: ")
+n2 = input("Digite o número 2: ")
+
+#Processamento
+soma = int(n1) + int(n2) 
+
+#Saída
+print("A soma é de ", n1, " + ", n2, " = ", soma)
+
+#int("10") -> converte o texto "10" para inteiro 10
+#float("1.5") -> converte o texto "1.5" para real 1.5
+#str(10) -> converte o inteiro 10 para texto "10"
